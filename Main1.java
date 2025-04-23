2d888b6586262fbe4e7ff8167413f1668e156ceb
Caito931 <chora2aum@gmail.com>
2025-04-22 21:54:55 -0400

    Lesson 1

--- Main1.java
+++ Main1.java
@@ -4,6 +4,33 @@ class Main1
 {
     public static void main(String[] args) 
     {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        // Declare
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
+
+        // Set
+        System.out.println("\nEnter an adjective ( description ): ");
+        adjective1 = scanner.nextLine();
+        System.out.println("Enter a noun (animal or person): ");
+        noun1 = scanner.nextLine();
+        System.out.println("Enter an adjective ( description ): ");
+        adjective2 = scanner.nextLine();
+        System.out.println("Enter a verb end with -ing (action): ");
+        verb1 = scanner.nextLine();
+        System.out.println("Enter an adjective ( description ): ");
+        adjective3 = scanner.nextLine();
+
+        // Final
+        System.out.println("Today I went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
+
+        scanner.close();
     }
 }
